5aea7387c7decf3fd80fa58c419f85ec11abaffe
Glenton Muwishi <gmuwishi@delta.co.zw>
2021-01-14 01:27:45 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const circle = {
+    // radius and location are properties
+    radius: 1,
+    location: {
+        x: 1,
+        x: 2
+    },
+    //draw is a method
+    draw: function(){
+        console.log('draw');
+    }
+};
+
+circle.draw();
