7662810e1e94262d3072a937ebd946eb04ffd0e0
Glenton Muwishi <gmuwishi@delta.co.zw>
2021-01-14 01:43:01 +0200

    first factory function

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const circle = {
-    // radius and location are properties
-    radius: 1,
-    location: {
-        x: 1,
-        x: 2
-    },
-    //draw is a method
-    draw: function(){
-        console.log('draw');
-    }
-};
-
+//factory function
+function createCircle(radius ){
+    return  {
+        radius,
+        draw: function(){
+            console.log('draw');
+        }
+    };
+}
+const circle = createCircle(1);
 circle.draw();
