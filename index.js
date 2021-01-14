730af9460fb2eba82b96d652561ad0b34b865f62
Glenton Muwishi <gmuwishi@delta.co.zw>
2021-01-14 02:31:38 +0200

    primitives vs objects

--- index.js
+++ index.js
@@ -17,5 +17,46 @@ function Circle(radius){
     }
 }
 
-//new operator creates a new object then sets this to the object and then returns the object from the function
+//new operator creates  a new object then sets this to the object and then returns the object from the function
 const anotherCircle = new Circle(1);
+
+ 
+
+//value vs reference types
+
+let x = 10;
+let y = x;
+
+x= 20;
+//x = 20
+console.log(x);
+//y = 10 because the values are independent of each other
+console.log(y);
+
+let a = {value: 10};
+let b =a;
+
+a.value = 20;
+// a = 20
+console.log(a);
+// b = 20 because an object is not stored in the variable. the address of the morty location of the object is stored in the variable 
+console.log(b);
+
+// primitives are copied by value and objects are copied by their reference e.g
+
+let number = 10;
+function increase(number){
+    //number = 11 only within this function
+    number++;
+}
+increase(number);
+// number = 10 since its copied by value
+console.log(number);
+
+let number2 = {value: 20};
+function increase(number2){
+    number2.value++;
+}
+increase(number2);
+// number = 21 since object is copied by value
+console.log(number2);
