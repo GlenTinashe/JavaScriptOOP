d3684f47081526a42adbcf2662c3a569559044e2
Glenton Muwishi <gmuwishi@delta.co.zw>
2021-01-14 02:03:11 +0200

    factory vs constructor functions

--- index.js
+++ index.js
@@ -8,4 +8,14 @@ function createCircle(radius ){
     };
 }
 const circle = createCircle(2);
-circle.draw();
+
+// Constructor function
+function Circle(radius){
+    this.radius = radius;
+    this.draw = function () {
+        console.log('draw');
+    }
+}
+
+//new operator creates a new object then sets this to the object and then returns the object from the function
+const anotherCircle = new Circle(1);
